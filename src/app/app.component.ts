565659ab34fc43c48ca6e2d262cab0ece6e40ce0
martin <martin.alvarado2002@gmail.com>
2024-04-02 16:29:19 +0200

    [28-452]: The void State

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -70,6 +70,29 @@ import {
         animate(500),
       ]),
     ]),
+    trigger('list1', [
+      state(
+        'in',
+        style({
+          opacity: 1,
+          transform: 'translateX(0)',
+        })
+      ),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)',
+        }),
+        animate(300),
+      ]),
+      transition('* => void', [
+        animate(300),
+        style({
+          transform: 'translateX(100px)',
+          opacity: 0,
+        }),
+      ]),
+    ]),
   ],
 })
 export class AppComponent {
@@ -81,7 +104,9 @@ export class AppComponent {
     this.list.push(item);
   }
 
-  onDelete(item: any) {}
+  onDelete(item: any) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
 
   onAnimate() {
     this.state = this.state === 'normal' ? 'highlighted' : 'normal';
